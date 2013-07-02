cb0e499646efd940d8631105d0d030d640c9e55d
riceje7 <riceje7@gmail.com>
2013-07-02 11:24:35 -0400

    deleted v2 folder

--- src/v2.0/js/modal-dismiss-code.js
+++
@@ -1,6 +0,0 @@
-//click handler for modal-dismiss-button, removes #modal-bg and #modal-wrapper from the document
-
-$('#modal-dismiss-button').click(function () {
-	$('#modal-wrapper').remove();
-	$('#modal-bg').remove();
-});
