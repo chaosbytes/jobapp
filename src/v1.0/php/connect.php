0434e28aa4cc226332acbcae0985c38aee02a9a5
Joseph Rice <riceje7@Zombie-Defense-League-HQ-2.local>
2013-07-06 11:25:02 -0400

    update connect.php

--- src/v1.0/php/connect.php
+++ src/v1.0/php/connect.php
@@ -2,7 +2,7 @@
 // connect.php - houses connect function for connecting to MySQL through mysqli
 function connect() {
 	//create new mysqli link
-	$link = new mysqli('localhost', 'root', 'aecolomjerice1024!', "jobapp_v1");
+	$link = new mysqli('mysql.jobapp.foursquaregames.com', 'riceje7', 'aecolomjerice1024!', "jobapp_v1");
 	// if there was an error
 	if ($link->connect_errno) {
 		//echo error response
